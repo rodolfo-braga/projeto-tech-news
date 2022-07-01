470d41bde8276026206c5adda1133edd15499494
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-01 14:25:31 -0300

    feat(scraper): implements function scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,11 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css('a.next::attr(href)').get()
+    if not next_page_link:
+        return None
+    return next_page_link
 
 
 # Requisito 4
