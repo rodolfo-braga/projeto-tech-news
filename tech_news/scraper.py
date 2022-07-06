edfb7a064e82bc4e315003adbec04f146fd0a6d9
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-06 11:53:21 -0300

    feat(scraper): implements function scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,9 +37,32 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css('h1.entry-title::text').get()
+    timestamp = selector.css('li.meta-date::text').get()
+    writer = selector.css('span.author a::text').get()
+    comments_count = len(selector.css('.comment').getall())
+    summary = selector.xpath('string(//div[@class="entry-content"]/p)').get()
+    tags = selector.css('.post-tags ul li a::text').getall()
+    category = selector.css('span.label::text').get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# Referências:
+# - Para selecionar o summary no requisito 4:
+# https://parsel.readthedocs.io/en/latest/usage.html#using-text-nodes-in-a-condition
