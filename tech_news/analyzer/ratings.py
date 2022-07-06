7798f6d4e50d967670055f4a8c4bbbca1117e8ad
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-06 19:14:58 -0300

    feat(ratings): implements function top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,13 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news_sorted = sorted(news, key=lambda x: x['comments_count'], reverse=True)
+    return [
+        (news['title'], news['url']) for news in news_sorted[:5]
+    ]
 
 
 # Requisito 11
